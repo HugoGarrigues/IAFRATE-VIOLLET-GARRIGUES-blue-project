ba53b00bd26eb98577ec1f7404d23c3892e47669
thomasiafrate1 <113009615+thomasiafrate1@users.noreply.github.com>
2023-09-11 16:10:56 +0200

    start frontend

--- src/backend/app.js
+++ src/backend/app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Ma biute');
 });
 
 app.listen(port, () => console.log('Server listening on port ' + port));
