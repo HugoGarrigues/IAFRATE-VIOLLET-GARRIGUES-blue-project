dda7b095db7ed58be4b8d586054e17466eb73822
yoanvlt <113009422+yoanvlt@users.noreply.github.com>
2023-09-11 16:38:29 +0200

    creation de la premiere route

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const port = 3000;
 
+app.use(express.static('../frontend'));
+
 app.get('/', (req, res) => {
-  res.send('Ma biute');
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
 });
 
 app.listen(port, () => console.log('Server listening on port ' + port));
-
