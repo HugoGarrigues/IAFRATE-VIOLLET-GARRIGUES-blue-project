b4575cd276b26910c2c69983a5e03188dfb97a6e
yoanvlt <113009422+yoanvlt@users.noreply.github.com>
2023-09-12 14:07:13 +0200

    items classe et bouton jouer

---
+++ src/backend/models/items.js
@@ -0,0 +1,10 @@
+class Items {
+    constructor(name,description,price,effect){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.effect = effect;
+    }
+}
+
+module.exports = Items;
