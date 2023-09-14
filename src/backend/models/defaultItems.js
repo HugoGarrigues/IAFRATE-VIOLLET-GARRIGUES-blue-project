b11993cfa60f068238b6cfd37e238c12ae9e4416
thomasiafrate1 <113009615+thomasiafrate1@users.noreply.github.com>
2023-09-14 12:56:19 +0200

    FIN

--- src/backend/models/defaultItems.js
+++ src/backend/models/defaultItems.js
@@ -1,11 +1,11 @@
 const Items = require('./items');
 
-const boussole = new Items('Boussole du Capitaine Jack','Permet au joueur de gagner de un peu d alignement',100,[{type:'alignement',value:10}]);
-const longuevue = new Items('Longue Vue','Permet au joueur de gagner de un peu d or',100,[{type:'gold',value:10}]);
-const epee = new Items('Epee','Permet de gagner de l alignement et de l or',100,[{type:'alignement',value:5},{type:'gold',value:5}]);
-const fusil = new Items('Fusil','Permet de gagner beaucoup d or',100,[]);
-const sniper = new Items('Sniper','Permet de gagner beaucoup d alignement ',100,[]);
-const pistolet = new Items('Tromblon','Permet de passer un évenement ',100,[]);
+const boussole = new Items('Boussole du Capitaine Jack','Permet au joueur de gagner de un peu d alignement',15,[{type:'alignement',value:10}]);
+const longuevue = new Items('Longue Vue','Permet au joueur de gagner de un peu d or',15,[{type:'gold',value:10}]);
+const epee = new Items('Epee','Permet de gagner de l alignement et de l or',15,[{type:'alignement',value:5},{type:'gold',value:5}]);
+const fusil = new Items('Fusil','Permet de gagner beaucoup d or',15,[]);
+const sniper = new Items('Sniper','Permet de gagner beaucoup d alignement ',15,[]);
+const pistolet = new Items('Tromblon','Permet de passer un évenement ',20,[]);
 
 
 module.exports = [boussole,longuevue,epee,fusil,sniper,pistolet];
